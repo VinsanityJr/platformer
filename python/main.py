9dc92ded794f46575aca1dc26b8bebe505d3f3c4
VinsanityJr <42324584+VinsanityJr@users.noreply.github.com>
2022-10-02 16:01:47 -0400

    Added Guidelines for Main file
    Added Comments to main file

--- python/main.py
+++ python/main.py
@@ -1,3 +1,10 @@
 import pygame
 
-# do main things here
+# the main loop needs to handle window creation and gamestate.
+
+# open a window here
+
+# audio logic
+
+# main render loop (I'll help here)
+# I'm not sure how to handle user input yet, but that's a later issue
