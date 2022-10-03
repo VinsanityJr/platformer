dbabb4b845ff3a6bf81801ce51dec27a8a9a9dbd
VinsanityJr <42324584+VinsanityJr@users.noreply.github.com>
2022-10-03 09:50:13 -0400

    Began implementing screen framework
    Began implementing a framework for the screen class.
    Added  a "drawable" class, though this name doesn't properly communicate what it will be used for. This should probably be renamed in the near future.

---
+++ python/render/drawables/drawable.py
@@ -0,0 +1,7 @@
+# Every drawable object on a screen object should be an instance of the
+# drawable class, so that the render loop of the screen can easily handle
+# it.
+
+class Drawable:
+    def __init__(self):
+        pass
---
+++ python/render/screen.py
@@ -0,0 +1,11 @@
+# The screen class provides a framework for adding different screens
+# to the game. Anything displayed to the user while viewing menus should
+# be an instance of the screen class.
+
+class Screen:
+    def __init__(self):
+        self.drawables = []
+
+    # Render all the drawable items in this screen
+    def render(self):
+        pass
